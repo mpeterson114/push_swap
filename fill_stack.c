a8d5e20a53a0332ed95522242347ff8deb3a7156
mpeterson114 <mlpeterson114@gmail.com>
2023-02-22 14:19:24 +0100

    indexes, main started

--- fill_stack.c
+++ fill_stack.c
@@ -24,13 +24,36 @@ t_stack	*fill_values(int argc, char **argv)
 			stack_add_back(&stack_a, stack_new(nb));
 	}
 	return (stack_a);
+	//printf("%d\n", stack_a->value);
 }
 
 void	assign_index(t_stack *stack_a, int stack_size)
 {
-	t_stack *top;
-	t_stack *temp;
-	int value;
+	t_stack	*ptr;
+	t_stack	*highest;
+	int		value;
 
-
+	while (--stack_size > 0)
+	{
+		ptr = stack_a;
+		value = INT_MIN;
+		highest = NULL;
+		while (ptr)
+		{
+			if (ptr->value == INT_MIN && ptr->index == 0)
+				ptr->index = 1;
+			if (ptr->value > value && ptr->index == 0)
+			{
+				value = ptr->value;
+				highest = ptr;
+				ptr = stack_a;
+			}
+			else
+				ptr = ptr->next;
+		}
+		if (highest != NULL)
+			highest->index = stack_size;
+	}
+	//printf("%d, ", highest->index);
+	//printf("%d\n", highest->value);
 }
